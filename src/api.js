9f6c8cc0b5e8dc92dc71740f80421a52c87678b6
Divya7094 <divyat51951@gmail.com>
2025-02-16 00:16:54 +0530

    Updated frontend changes

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./utils/auth";
 
 const API = axios.create({
-  baseURL: "https://real-time-chat-backend.onrender.com",  // ✅ Replace with actual Render backend URL
+  baseURL: "https://real-time-chat-app-backend-production.up.railway.app",  // ✅ Replace with actual Render backend URL
 });
 
 API.interceptors.request.use((config) => {
