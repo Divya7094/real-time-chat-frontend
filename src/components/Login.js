ac730a255fcc318e634bc29e9f1419e066b20c61
Divya7094 <divyat51951@gmail.com>
2025-02-15 19:54:28 +0530

    Updated frontend changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
     try {
       console.log("🔹 Sending Login Request:", { username, password });
 
-      const response = await fetch("https://real-time-chat-app-backend-l2sp.onrender.com//login", {
+      const response = await fetch("https://real-time-chat-app-backend-l2sp.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
